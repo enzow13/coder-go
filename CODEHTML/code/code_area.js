d9505b52aa87ef15b15a1433566d8cad9e501efb
Enzo Seschi <enzosseschi@hotmail.com>
2023-10-26 23:31:32 +0000

    Atualizando CodeMirror

--- CODEHTML/code/code_area.js
+++ CODEHTML/code/code_area.js
@@ -1,7 +1,33 @@
+let textarea = document.getElementById("Input Code");
+let btnRun = document.getElementById("btn-run");
+
+/* Objeto do textarea código */
+
+var code = CodeMirror.fromTextArea(textarea, {
+  mode: 'javascript',
+  theme: 'default',
+  lineNumbers: true
+});
+
+/* Botão que recebe valores do objeto textarea e retorna em compilado */
+
+btnRun.addEventListener('click', () => {
+  var codigo = code.getValue();
+
+  console.log(eval(`${codigo}`))
+})
+
+
+/*
 document.addEventListener('DOMContentLoaded', function() {
-    CodeMirror.fromTextArea(document.getElementById("CODE-AREA"), {
-      mode: 'python',
-      theme: 'darcula',
+    let code = CodeMirror.fromTextArea(codigo, {
+      mode: 'javascript',
+      theme: 'default',
       lineNumbers: true
     });
   });
+
+document.getElementById("btn-run").addEventListener('click', () => {
+  code.getValue();
+})
+*/
